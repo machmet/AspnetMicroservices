10edf09c59717234ce76859b4dab851455531d4b
Michal Metkowski <machmet@gmail.com>
2022-04-23 22:50:27 +0200

    Init project

---
+++ src/Catalog/Catalog.API/Controllers/WeatherController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/weather")]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
